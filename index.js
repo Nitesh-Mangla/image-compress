84c2c900ffaae85f8013cd745ead0721b457959f
Nitesh Mangla <niteshmangla@Niteshs-MacBook-Air.local>
2025-03-09 14:55:11 +0530

    heath check apis

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const server = require("http");
 const cors = require("cors");
-const path = require("path");
 const app  = express();
 require('dotenv').config();
 const compressorRoute = require('./routes/api.js');
@@ -12,12 +10,12 @@ app.use('/', webRouter);
 app.use(express.static('frontend'));
 app.use(cors());
 
-const httpServer = server.createServer(app);
+// const httpServer = server.createServer(app);
 
 app.get('/health', (req, res) => {
     res.send('Server is running...');
 });
 
-httpServer.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+app.listen(3031, () => {
+    console.log(`Server started on port 3031`);
 });
